1423edf373fcaca857be6e44f81509860454dea2
wayne930242 <wayne930242@gmail.com>
2021-11-12 19:01:53 +0800

    add robot block and remove swr package

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,9 @@ class MyDocument extends Document {
     return (
       <Html lang="zh-tw">
         <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="robots" content="noindex,nofollow" />
         </Head>
         <body>
           <Main />
